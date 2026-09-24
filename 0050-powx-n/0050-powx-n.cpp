5570b644240fe74936575ab011d20e1983cdcc78
init_rohit_14 <herojaiswal235@gmail.com>
2026-09-24 19:28:36 +0530

    Time: 0 ms (100%), Space: 8.6 MB (71.78%) - LeetHub

---
+++ 0050-powx-n/0050-powx-n.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    double myPow(double x, long long n) {
+        if (n == 0)
+            return 1;
+
+        if (n < 0)
+            return 1 / myPow(x, -n);
+
+        double ans = myPow(x, n / 2);
+
+        if (n % 2 == 0)
+            return ans * ans;
+        
+        return x * ans * ans;
+    }
+};
